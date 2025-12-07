0062705048aa36391359086fb43948e8f4c81e5a
Tu Dinh <contact@tudinh.xyz>
2025-12-07 18:20:55 +0100

    Simplify roundtrip.tests.ps1
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

--- tests/roundtrip.tests.ps1
+++ tests/roundtrip.tests.ps1
@@ -15,19 +15,31 @@ Describe "roundtrip container tests" -ForEach @(
 ) {
     . $PSScriptRoot/../fixtures/testdir.ps1
 
-    It "compresses and decompresses zip" -ForEach @(
-        @{ CompressOptions = @() }
-        @{ CompressOptions = @("-mm=Copy") }
-        @{ CompressOptions = @("-mm=Deflate") }
-        @{ CompressOptions = @("-mm=Deflate64") }
-        @{ CompressOptions = @("-mm=BZip2") }
-        @{ CompressOptions = @("-mm=LZMA") }
-        @{ CompressOptions = @("-mm=PPMd") }
+    It "compresses and decompresses" -ForEach @(
+        @{ Extension = ".zip"; CompressOptions = @() }
+        @{ Extension = ".zip"; CompressOptions = @("-mm=Copy") }
+        @{ Extension = ".zip"; CompressOptions = @("-mm=Deflate") }
+        @{ Extension = ".zip"; CompressOptions = @("-mm=Deflate64") }
+        @{ Extension = ".zip"; CompressOptions = @("-mm=BZip2") }
+        @{ Extension = ".zip"; CompressOptions = @("-mm=LZMA") }
+        @{ Extension = ".zip"; CompressOptions = @("-mm=PPMd") }
+        @{ Extension = ".7z"; CompressOptions = @() }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=LZMA") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=LZMA2") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=PPMd") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=BZip2") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=Deflate") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=Copy") }
+        @{ Extension = ".wim"; CompressOptions = @() }
+        @{ Extension = ".tar"; CompressOptions = @() }
+        @{ Extension = ".tar"; CompressOptions = @("-mm=gnu") }
+        @{ Extension = ".tar"; CompressOptions = @("-mm=pax") }
+        @{ Extension = ".tar"; CompressOptions = @("-mm=posix") }
     ) {
         Invoke-Roundtrip `
             -NanaZip $NanaZip `
             -InputFile "$InputFile/*" `
-            -CompressedFile "$TestDrive/compressed/test.zip" `
+            -CompressedFile "$TestDrive/compressed/test$Extension" `
             -ExtractedDir "$TestDrive/extracted" `
             -CompressOptions $CompressOptions `
             -Verbose:$VerbosePreference
@@ -39,96 +51,19 @@ Describe "roundtrip container tests" -ForEach @(
             -ActualDir "$TestDrive/extracted"
     }
 
-    It "compresses and decompresses zip (zstd)" -Tag "7-Zip-zstd" -ForEach @(
-        @{ CompressOptions = @("-mm=zstd") }
+    It "compresses and decompresses (7-Zip-zstd)" -Tag "7-Zip-zstd" -ForEach @(
+        @{ Extension = ".zip"; CompressOptions = @("-mm=zstd") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=FLZMA2") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=zstd") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=Brotli") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=LZ4") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=LZ5") }
+        @{ Extension = ".7z"; CompressOptions = @("-m0=Lizard") }
     ) {
         Invoke-Roundtrip `
             -NanaZip $NanaZip `
             -InputFile "$InputFile/*" `
-            -CompressedFile "$TestDrive/compressed/test.zip" `
-            -ExtractedDir "$TestDrive/extracted" `
-            -CompressOptions $CompressOptions `
-            -Verbose:$VerbosePreference
-        Compare-TestDirFile `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-        Compare-TestDirMtime `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-    }
-
-    It "compresses and decompresses 7z" -ForEach @(
-        @{ CompressOptions = @() }
-        @{ CompressOptions = @("-m0=LZMA") }
-        @{ CompressOptions = @("-m0=LZMA2") }
-        @{ CompressOptions = @("-m0=PPMd") }
-        @{ CompressOptions = @("-m0=BZip2") }
-        @{ CompressOptions = @("-m0=Deflate") }
-        @{ CompressOptions = @("-m0=Copy") }
-    ) {
-        Invoke-Roundtrip `
-            -NanaZip $NanaZip `
-            -InputFile "$InputFile/*" `
-            -CompressedFile "$TestDrive/compressed/test.7z" `
-            -ExtractedDir "$TestDrive/extracted" `
-            -CompressOptions $CompressOptions `
-            -Verbose:$VerbosePreference
-        Compare-TestDirFile `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-        Compare-TestDirMtime `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-    }
-
-    It "compresses and decompresses 7z (7-Zip ZS)" -Tag "7-Zip-zstd" -ForEach @(
-        @{ CompressOptions = @("-m0=FLZMA2") }
-        @{ CompressOptions = @("-m0=zstd") }
-        @{ CompressOptions = @("-m0=Brotli") }
-        @{ CompressOptions = @("-m0=LZ4") }
-        @{ CompressOptions = @("-m0=LZ5") }
-        @{ CompressOptions = @("-m0=Lizard") }
-    ) {
-        Invoke-Roundtrip `
-            -NanaZip $NanaZip `
-            -InputFile "$InputFile/*" `
-            -CompressedFile "$TestDrive/compressed/test.7z" `
-            -ExtractedDir "$TestDrive/extracted" `
-            -CompressOptions $CompressOptions `
-            -Verbose:$VerbosePreference
-        Compare-TestDirFile `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-        Compare-TestDirMtime `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-    }
-
-    It "compresses and decompresses wim" {
-        Invoke-Roundtrip `
-            -NanaZip $NanaZip `
-            -InputFile "$InputFile/*" `
-            -CompressedFile "$TestDrive/compressed/test.wim" `
-            -ExtractedDir "$TestDrive/extracted" `
-            -CompressOptions @() `
-            -Verbose:$VerbosePreference
-        Compare-TestDirFile `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-        Compare-TestDirMtime `
-            -ExpectedDir "$InputFile" `
-            -ActualDir "$TestDrive/extracted"
-    }
-
-    It "compresses and decompresses tar" -ForEach @(
-        @{ CompressOptions = @("-mm=gnu") }
-        @{ CompressOptions = @("-mm=pax") }
-        @{ CompressOptions = @("-mm=posix") }
-    ) {
-        Invoke-Roundtrip `
-            -NanaZip $NanaZip `
-            -InputFile "$InputFile/*" `
-            -CompressedFile "$TestDrive/compressed/test.tar" `
+            -CompressedFile "$TestDrive/compressed/test$Extension" `
             -ExtractedDir "$TestDrive/extracted" `
             -CompressOptions $CompressOptions `
             -Verbose:$VerbosePreference
