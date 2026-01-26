5302e3bd8688ae7a4d6e67320f28bf9a5568fa2f
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-26 23:27:26 +0800

    Move Assets/TestData/NanaZip.Tests.Samples to Assets/TestData/NanaZip.Codecs.Samples.

--- tests/samples.tests.ps1
+++ tests/samples.tests.ps1
@@ -13,10 +13,10 @@ Describe "NanaZip-specific container tests" -Tag "NanaZip" {
     . $PSScriptRoot/../fixtures/testdir.ps1
 
     It "parses the archive" -ForEach @(
-        @{ InputFile = "$AssetsDir/TestData/NanaZip.Tests.Samples/Electron.Sample.asar" }
-        # @{ InputFile = "$AssetsDir/TestData/NanaZip.Tests.Samples/ROMFS.Sample.img" }
-        @{ InputFile = "$AssetsDir/TestData/NanaZip.Tests.Samples/WebAssembly.Sample.wasm" }
-        @{ InputFile = "$AssetsDir/TestData/NanaZip.Tests.Samples/ZealFS.V1.Sample.img" }
+        @{ InputFile = "$AssetsDir/TestData/NanaZip.Codecs.Samples/Electron.Sample.asar" }
+        # @{ InputFile = "$AssetsDir/TestData/NanaZip.Codecs.Samples/ROMFS.Sample.img" }
+        @{ InputFile = "$AssetsDir/TestData/NanaZip.Codecs.Samples/WebAssembly.Sample.wasm" }
+        @{ InputFile = "$AssetsDir/TestData/NanaZip.Codecs.Samples/ZealFS.V1.Sample.img" }
     ) {
         $Output = & $Program t $InputFile 2>&1
         $ExitCode = $LASTEXITCODE
