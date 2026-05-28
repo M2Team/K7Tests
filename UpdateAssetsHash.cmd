2f51f4661815294b2c0ec634513b1b5fcd640aba
Tu Dinh <contact@tudinh.xyz>
2026-05-28 22:21:00 +0200

    Set LC_ALL=C in UpdateAssetsHash.cmd
    Certain environments (e.g. VSCode PowerShell) can set the LANG variable,
    which will influence the sort order. Override this before running sort
    to ensure stable ordering of asset names in Assets.sha256.
    
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

--- UpdateAssetsHash.cmd
+++ UpdateAssetsHash.cmd
@@ -2,6 +2,7 @@
 @echo off
 
 pushd "%~dp0Assets\"
+set LC_ALL=C
 MSYS2\find.exe 7-Zip/ Fuzzing/ MSYS2/ TestData/ -type f -print | MSYS2\sort.exe | MSYS2\xargs.exe -d '\n' MSYS2\sha256sum.exe -t > Assets.sha256
 popd
 
